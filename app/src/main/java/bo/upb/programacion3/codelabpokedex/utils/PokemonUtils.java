3360bb5c9d8f4c14bb0a49a6c774903bc0183efc
Denisse <denissebaldiviesopacheco@gmail.com>
2020-06-22 19:01:04 -0400

    LISTOO!

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -26,6 +26,16 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13,"Purple",R.drawable.a,"Adorable"));
+        pokemons.add(new Pokemon(14,"Jiggly puff",R.drawable.aaa,"Super Adorable"));
+        pokemons.add(new Pokemon(15,"Mimikiu",R.drawable.aa,"Belleza"));
+        pokemons.add(new Pokemon(16,"Tyranitar",R.drawable.aaaa,"Dinosaurio?"));
+        pokemons.add(new Pokemon(17,"Newsletter",R.drawable.aaaaa,"Normal"));
+        pokemons.add(new Pokemon(18,"Magikarp",R.drawable.aaaaaa,"Pez?"));
+        pokemons.add(new Pokemon(19,"Celebi",R.drawable.aaaaaaa,"Miedo"));
+        pokemons.add(new Pokemon(20,"Jiggly",R.drawable.aaaaaaaa,"Evolucion?"));
+        pokemons.add(new Pokemon(21,"Wartortle",R.drawable.aaaaaaaaa,"Evolucion?"));
+        pokemons.add(new Pokemon(22,"Eevee",R.drawable.aaaaaaaaaa,"Adorable"));
         return pokemons;
     }
 }
